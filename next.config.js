280a7c36e258abe088c9922226a7de76aa8f5f12
Abdul Shakoor <abdul.shakoor@reavertechnologies.com>
2025-11-21 21:01:13 +0500

    Configure path alias for @/components

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    // Add alias for '@' to point to the 'src' or 'app' directory
+    config.resolve.alias['@'] = __dirname + '/app';
+    return config;
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
